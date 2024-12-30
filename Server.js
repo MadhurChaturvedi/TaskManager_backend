249e5748a1e1ad7fac6e12e976d6d37c0a49669e
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2024-12-30 21:00:05 +0530

    resolve cors

--- Server.js
+++ Server.js
@@ -10,7 +10,12 @@ const { errorHandler } = require('./middleware/ErrorMiddleware.js')
 const userRouter = require('./routes/userRoutes.js')
 const cors = require('cors')
 
-app.use(cors())
+app.use(cors(
+    {
+        origin: 'https://taskmanager-backend-9taa.onrender.com',
+        optionsSuccessStatus: 200
+    }
+))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
